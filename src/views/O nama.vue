c2fd916ddcfaa99e9bae783000f329495d0ad96d
mateorumac <mateo.rumac@gmail.com>
2023-01-29 12:33:23 +0100

    O nama raspored

--- src/views/O nama.vue
+++ src/views/O nama.vue
@@ -1,3 +1,8 @@
 <template>
-  
+<p>Salon ljepote Ana nalazi se u Puli. <br>
+ Našim klijentima nudimo usluge njege lica, masaže, depilacije, njege ruku, oblikovanja tijela, a imamo i odjel prodaje preparativne i dekorativne kozmetike.</p>
+<p>Naš salon opremljen je po visokim standardima, pružajući klijentima ugodnu i toplu atmosferu. <br> Svi naši klijenti dobiju najbolju profesionalnu uslugu za što se brine naš stručni i talentirani tim. <br> Naš uspjeh dugujemo konstantnom poboljšavanju vlastitih usluga i ono najvažnije, iznimnom zadovoljstvu naših klijenata.</p>
+<p>Slika 1</p>
+<p>Slika 2</p>
+<p>Slika 3</p>
 </template>
