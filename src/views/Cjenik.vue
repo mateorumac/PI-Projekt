485acf11da933b877e0c05cbc8425880aae761e6
mateorumac <mateo.rumac@gmail.com>
2023-01-31 23:58:42 +0100

    rad na o nam podstranici

--- src/views/Cjenik.vue
+++ src/views/Cjenik.vue
@@ -1,3 +1,65 @@
 <template>
-  
-</template>
+
+    <div id="app">
+      
+
+      <div class="container1">
+        <div class="row">
+          
+
+          <div class="column main">
+            <!-- Add your main content here -->
+            <h2>{{ message }}</h2>
+            <p>This is a Vue.js website example. On small screens, the menu and right column will stack on top of each other.</p>
+          </div>
+
+          <div class="column right">
+            <!-- Add any additional content here -->
+            <p>right</p>
+          </div>
+        </div>
+      </div>
+
+      
+    </div>
+    </template>
+<style>
+      * {
+        box-sizing: border-box;
+      }
+      .container1 {
+        max-width: 960px;
+        margin: 0 auto;
+      }
+      .header,
+      .footer {
+        background-color: #f2f2f2;
+        padding: 10px;
+        text-align: center;
+      }
+      .header {
+        position: fixed;
+        top: 0;
+        width: 100%;
+      }
+      
+      .row::after {
+        content: "";
+        clear: both;
+        display: table;
+      }
+      .column {
+        float: left;
+        width: 100%;
+        padding: 10px;
+      }
+      
+      @media (max-width: 600px) {
+        .menu,
+        .main,
+        .right {
+          width: 100%;
+        }
+      }
+    </style>
+
--- src/views/O nama.vue
+++ src/views/O nama.vue
@@ -1,8 +1,67 @@
 <template>
-<p>Salon ljepote Ana nalazi se u Puli. <br>
+
+    <div id="app">
+      
+
+      <div class="container1">
+        <div class="row">
+          
+
+          <div class="column main">
+            <!-- Add your main content here -->
+            <img src="salon2.jpg" alt="salon" style="width: 80%; margin-left:-5%">
+            <p>Salon ljepote Ana nalazi se u Puli. <br>
  Našim klijentima nudimo usluge njege lica, masaže, depilacije, njege ruku, oblikovanja tijela, a imamo i odjel prodaje preparativne i dekorativne kozmetike.</p>
-<p>Naš salon opremljen je po visokim standardima, pružajući klijentima ugodnu i toplu atmosferu. <br> Svi naši klijenti dobiju najbolju profesionalnu uslugu za što se brine naš stručni i talentirani tim. <br> Naš uspjeh dugujemo konstantnom poboljšavanju vlastitih usluga i ono najvažnije, iznimnom zadovoljstvu naših klijenata.</p>
-<p>Slika 1</p>
-<p>Slika 2</p>
-<p>Slika 3</p>
-</template>
+ <img src="salon3.jpg" alt="salon" style="width: 80%;">
+<p>Naš salon opremljen je po visokim standardima, pružajući klijentima ugodnu i toplu atmosferu. 
+    <br> Svi naši klijenti dobiju najbolju profesionalnu uslugu za što se brine naš stručni i talentirani tim. 
+    <br> Naš uspjeh dugujemo konstantnom poboljšavanju vlastitih usluga i ono najvažnije, iznimnom zadovoljstvu naših klijenata.</p>
+          </div>
+
+          <div class="column right">
+            <!-- Add any additional content here -->
+            <img src="salon1.jpg" alt="salon" style="width: 80%;">
+          </div>
+        </div>
+      </div>
+
+      
+    </div>
+    </template>
+<style>
+      * {
+        box-sizing: border-box;
+      }
+      .container1 {
+        max-width: 960px;
+        margin: 0 auto;
+      }
+    
+      
+      .row::after {
+        content: "";
+        clear: both;
+        display: table;
+      }
+      .column {
+        float: left;
+        width: 100%;
+        padding: 10px;
+      }
+      
+      @media (max-width: 600px) {
+        
+        .main,
+        .right {
+          width: 100%;
+        }
+      }
+
+      p{
+        margin:5%;
+        padding-top: 5%;
+      }
+    </style>
+
+
+
