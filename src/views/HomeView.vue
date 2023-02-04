37cdf14e335b534ac2732fe0519e2d1fa2f069b5
mateorumac <mateo.rumac@gmail.com>
2023-02-05 00:34:21 +0100

    introduced carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,90 @@
 <template>
-    <div id="app">
+<div class="slider">
+  <div class="slide-track">
+    <!--     ------5 img---- -->
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/8066167/pexels-photo-8066167.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" href="">
+      <div class="details">
+        <div class="content">
+          <h1>heading</h1>
+          <p>this is a paragrapgh. nice to meet u dear. </p>
+        </div>
+      </div>
+
+    </div>
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/4348323/pexels-photo-4348323.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500">
+      <div class="details">
+        <div class="content">
+          <h1>heading</h1>
+          <p>this is a paragrapgh. nice to meet u dear. </p>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/7419661/pexels-photo-7419661.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500">
+      <div class="details">
+        <div class="content">
+          <h1>heading</h1>
+          <p>this is a paragrapgh. nice to meet u dear. </p>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/4999940/pexels-photo-4999940.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500">
+      <div class="details">
+        <div class="content">
+          <h1>heading</h1>
+          <p>this is a paragrapgh. nice to meet u dear. </p>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/7223805/pexels-photo-7223805.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500">
+      <div class="details">
+        <div class="content">
+          <h1>heading</h1>
+          <p>this is a paragrapgh. nice to meet u dear. </p>
+        </div>
+      </div>
+    </div>
+
+    <!--     ----same 5 img---- -->
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/8066167/pexels-photo-8066167.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500">
+      <div class="details">
+        <div class="content">
+          <h1>heading</h1>
+          <p>this is a paragrapgh. nice to meet u dear. </p>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/4348323/pexels-photo-4348323.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500">
+    </div>
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/7419661/pexels-photo-7419661.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500">
+      <div class="details">
+        <div class="content">
+          <h1>heading</h1>
+          <p>this is a paragrapgh. nice to meet u dear. </p>
+        </div>
+      </div>
+    </div>
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/4999940/pexels-photo-4999940.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500">
+    </div>
+    <div class="slide">
+      <img src="https://images.pexels.com/photos/7223805/pexels-photo-7223805.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500">
+      <div class="details">
+        <div class="content">
+          <h1>heading</h1>
+          <p>this is a paragrapgh. nice to meet u dear. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
       <div class="container1">
         <div class="row">
           
@@ -22,7 +107,7 @@
           </div>
         </div>
       </div>     
-    </div>
+    
     </template>
 <style>
       * {
@@ -30,7 +115,7 @@
         
       }
       .container1 {
-        
+        padding-top: 5%;
         max-width: 100%;
         margin: 0 auto;
         
@@ -64,6 +149,90 @@
         height: auto;
           }
 
+      
+.slider {
+  padding-top: 5%;  
+  padding-bottom: 5%;
+  height: 320px;
+  margin: auto;
+  position: relative;
+  width: 65%;
+  display: grid;
+  place-items: center;
+  overflow: hidden;
+}
+.slide-track {
+  display: flex;
+  width: calc(250px * 10);
+  animation: scroll 40s linear infinite;
+}
+@keyframes scroll {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(-250px * 5));
+  }
+}
+.slide {
+  height: 200px;
+  width: 250px;
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  perspective: 100px;
+}
+img {
+  width: 100%;
+  transition: transform 1s;
+}
+img:hover {
+  transform: translateZ(20px);
+}
+.slider::before,
+.slider::after {
+  background: linear-gradient(
+    to right,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  content: " ";
+  height: 100%;
+  position: absolute;
+  width: 15%;
+  z-index: 2;
+}
+.slider::before {
+  left: 0;
+  top: 0;
+}
+.slider::after {
+  right: 0;
+  top: 0;
+  transform: rotateZ(180deg);
+}
+.details {
+  position: absolute;
+  top: 10px;
+  left: 25px;
+  bottom: 10px;
+  right: 10px;
+  width: 200px;
+  background: rgba(0, 0, 0, 0);
+  transform: scaleY(0);
+  transition: transform 0.5s;
+}
+.slide:hover .details {
+  transform: scaleY(1);
+}
+.details .content {
+  position: absolute;
+  top: 0%;
+  transform: translateY(8%);
+  text-align: center;
+  color: white;
+}    
+
       @media (max-width: 600px) {
         
         .left,
@@ -72,8 +241,5 @@
         }
       }
 
-      
     </style>
-
-
 
