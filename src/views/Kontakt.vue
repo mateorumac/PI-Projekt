3b1e978ec6891ebf6ac0de0cab582f02f4645dc0
mateorumac <mateo.rumac@gmail.com>
2023-02-07 19:23:22 +0100

    mapa responzivna

--- src/views/Kontakt.vue
+++ src/views/Kontakt.vue
@@ -6,7 +6,7 @@
                 <div class="gmap_canvas">
                     <iframe width="600" height="600" 
                     id="gmap_canvas" src="https://maps.google.com/maps?q=Trg%20Kralja%20Tomislava%208%20Pula&t=&z=19&ie=UTF8&iwloc=&output=embed" 
-                    frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
             
                 </div>
             </div>
@@ -29,7 +29,7 @@
         max-width: 100%;
         margin: 0 auto;
         padding-bottom: 4%;
-        
+        overflow: hidden;
       }
     
       .row::after {
@@ -43,12 +43,30 @@
         padding: 10px;
       }
 
-.mapouter{
-    padding-left: 10%;
-    position:relative;
-    text-align:right;
-    height:600px;
-    width:600px;}
+      .mapouter{
+        padding-left: 10%;
+        position:relative;
+        text-align:right;
+        height: 0;
+        padding-bottom: 56%;
+        margin-left: 7%;
+        margin-right: 7%;
+      }
+
+      .gmap_canvas {
+  overflow: hidden;
+  background: none !important;
+  height: 100%;
+  width: 100%;
+}
+
+.gmap_canvas iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100% !important;
+  height: 100% !important;
+}
 @media (max-width: 600px) {
   .container {
     flex-direction: column;
